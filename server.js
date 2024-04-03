bbfdac04bc8dbdca34f2d634870c03b8ee56e045
distractabee <jssclgh@gmail.com>
2024-04-03 17:40:26 -0500

    added code to server js file hopefully now will use the routes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // require dependencies and make port
 const express = require("express");
-const fs = require("fs");
+const routes = require("./routes")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//require routes
 
 // use express, use routes
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(express.statis("public"));
+app.use(routes);
+// do the listening thing 
 
-// do the listening thing (add console.log(""))
+app.listen(PORT, () => {
+    console.log(`Now listening @ http://localhost:${PORT}`)
+})
