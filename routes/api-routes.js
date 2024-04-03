f01835a049e9b32227482d7feae17e2cc703719e
distractabee <jssclgh@gmail.com>
2024-04-02 19:44:06 -0500

    Added API routes (have not tested yet)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,19 @@
-// get /api/notes should read the db.json file and return all saved notes as JSON
-
+const db = require("../db/db.json");
+const fs = require('fs');
+const router = require("express").Router();
+// the uuid package will generate unique ids for each note
+const { uuid } = require('uuid');
 // post api/notes should receive a new note to save on the request body, 
 // add it to the db.json file, and return the new note to the client.
-// Find a way to give each note a unique id when it's saved
 
+router.get("/api/notes", (req, res) => {
+    readFromFile("./db/db.json")
+    .then((data) => res
+    .json(JSON.parse(data)))
+})
+
+router.post("/api/notes", (req, res) => {
+
+})
 // BONUS: add a delete /api/notes/:id route
+module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,5 @@
+const path = require("node:path");
+
 // get /notes should return the notes.html file
 
 // get * should return index.html
