9cde00418f7b23641939a11f3ec1ded367a8049d
distractabee <jssclgh@gmail.com>
2024-04-11 18:32:50 -0500

    fix html route??????

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,14 @@ const path = require("node:path");
 const router = require("express").Router();
 
 // returns the index.html file
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+})
+
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-})
+
 
 module.exports = router;
